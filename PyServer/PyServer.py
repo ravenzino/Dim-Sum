a9e69ea3ce859982263d75d64aeaa2e117ef7292
Raven Zino <raven.zino@gmail.com>
2015-06-05 20:31:39 +0800

    Server side code restructuring and optimization

--- PyServer/PyServer.py
+++ PyServer/PyServer.py
@@ -23,44 +23,51 @@ logger.setLevel(logging.DEBUG)
 logger.info('%s: Initiating...', time.ctime())
 
 class MyHTTPServer(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        logger.debug("----- %s [GET]------", time.ctime())
+
+    body = ''
+    bodyLength = None
+
+    def parse_request(self):
+        if False == http.server.BaseHTTPRequestHandler.parse_request(self):
+            return False
+        self.bodyLength = self.headers.get('Content-Length')
+        if self.bodyLength != None :
+            self.body = self.rfile.read(int(self.bodyLength)).decode('utf-8')
+
+        return True
+
+
+    def logHeader(self):
         logger.debug("client address: %s", self.client_address)
         logger.debug("client command: %s", self.command)
         logger.debug("client request path: %s", self.path)
         logger.debug("client request version: %s", self.request_version)
         #print("client request: ", self.requestline)
         logger.debug("client request headers: \n%s", self.headers)
-        length = self.headers.get('Content-Length')
-        if length != None :
-            logger.debug("length: %s", length)
-            logger.debug("client message: %s", self.rfile.read(int(length)).decode('utf-8'))
-        self.send_response(200)
-        #self.send_header('Time', 'Now!')
-        self.end_headers()
+        #length = self.headers.get('Content-Length')
+        if self.bodyLength != None :
+            logger.debug("length: %s", self.bodyLength)
+            logger.debug("client message: %s", self.body)
+
+    def do_GET(self):
+        logger.debug("----- %s [GET]------", time.ctime())
+        self.logHeader()
         outmessage = time.ctime().encode('utf-8')
+        self.send_response(200)
+        self.end_headers()
         self.wfile.write(outmessage)
         logger.debug("OUT: %s", outmessage)
 
     def do_POST(self):
         logger.debug("----- %s [POST]------", time.ctime())
-        logger.debug("client address: %s", self.client_address)
-        logger.debug("client command: %s", self.command)
-        logger.debug("client request path: %s", self.path)
-        logger.debug("client request version: %s", self.request_version)
-        #print("client request: ", self.requestline)
-        logger.debug("client request headers: \n%s", self.headers)
-        length = self.headers.get('Content-Length')
-        cmessage = ''
-        if length != None :
-            logger.debug("length: %s", length)
-            cmessage = self.rfile.read(int(length)).decode('utf-8')
-            logger.debug("client message: %s", cmessage)
-            cmessage = ("'%s' received" % cmessage).encode('utf-8')
+        self.logHeader()
+        outmessage = ''
+        if self.bodyLength != None :
+            outmessage = ("'%s' received" % self.body).encode('utf-8')
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(cmessage)
-        logger.debug("OUT: %s", cmessage)
+        self.wfile.write(outmessage)
+        logger.debug("OUT: %s", outmessage)
 
 httpd = http.server.HTTPServer(('', 8000), MyHTTPServer)
 httpd.serve_forever()
